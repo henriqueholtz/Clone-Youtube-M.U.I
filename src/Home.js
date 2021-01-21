07dca99a2bfa63a2d67f25c3f5da9fdbd311214b
HENRIQUE <henrique_holtz@hotmail.com>
2021-01-21 01:09:40 -0300

    Adding others items of left NavBar

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-import {makeStyles, AppBar, Toolbar, IconButton, Button, List, Drawer, Divider, ListItem, ListItemIcon, ListItemText, Box, Typography } from "@material-ui/core";
+import {makeStyles, AppBar, Toolbar, IconButton, Button, List, Drawer, Divider, ListItem, ListItemIcon, ListItemText, Box, Typography, ListSubheader } from "@material-ui/core";
 
 import { useTheme } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
@@ -56,6 +56,9 @@ const useStyles = makeStyles((theme) => ({
   },
   list: {
     marginTop: 52
+  },
+  subheader: {
+    textTransform: 'uppercase'
   }
 }));
 
@@ -99,7 +102,7 @@ function Home() {
         >
           <MoreVert />
         </IconButton>
-        <Button variant="outlined" color="secondary" startIcon={<AccountCircle />}>Fazer Login</Button>
+        <Button variant="outlined" color="secondary" startIcon={<AccountCircle />}>Sign In</Button>
       </Toolbar>
     </AppBar>
 
@@ -120,11 +123,11 @@ function Home() {
               }} primary={'Home'} />
           </ListItem>
         
-          <ListItem button key={'tendencies'} classes={{root: classes.listItem }}>
+          <ListItem button key={'trending'} classes={{root: classes.listItem }}>
             <ListItemIcon>{<Whatshot />}</ListItemIcon>
             <ListItemText classes={{
               primary: classes.listItemText
-              }} primary={'Tendencies'} />
+              }} primary={'Trending'} />
           </ListItem>
       
           <ListItem button key={'subscriptions'} classes={{root: classes.listItem }}>
@@ -137,7 +140,7 @@ function Home() {
 
         <Divider /> 
 
-        <List className={classes.list}>
+        <List>
           <ListItem button key={'library'} classes={{root: classes.listItem }}>
             <ListItemIcon>{<VideoLibrary />}</ListItemIcon>
             <ListItemText classes={{
@@ -152,6 +155,152 @@ function Home() {
               }} primary={'History'} />
           </ListItem>
         </List>
+
+        <Divider />
+
+        <Box p={3}>
+            <Typography variant="body2">
+                Sign in to like videos, comment and subscribe.
+            </Typography>
+            <Box mt={2}>
+              <Button 
+                variant="outlined"
+                color="secondary"
+                startIcon={<AccountCircle />}
+              >
+                Sign In
+              </Button>
+            </Box>
+        </Box>
+
+        <Divider /> 
+        
+        <List component="nav" aria-labelledby="nested-list-subheader" subheader={
+              <ListSubheader component="div" id="nested-list-subheader" className={classes.subheader}>
+                Best of YouTube
+              </ListSubheader>
+        }>
+          
+          <ListItem button key={'music'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Music'}/>
+          </ListItem>
+
+          <ListItem button key={'gaming'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Gaming'}/>
+          </ListItem>
+
+          <ListItem button key={'movies'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Movies'}/>
+          </ListItem>
+
+          <ListItem button key={'news'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'News'}/>
+          </ListItem>
+
+          <ListItem button key={'live'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Live'}/>
+          </ListItem>
+
+          <ListItem button key={'learning'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Learning'}/>
+          </ListItem>
+
+          <ListItem button key={'spotlight'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'Spotlight'}/>
+          </ListItem>
+
+          <ListItem button key={'360-video'} classes={{root: classes.listItem }}>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText classes={{
+              primary: classes.listItemText
+              }} primary={'360° Video'}/>
+          </ListItem>
+        </List>
+
+        <Divider />
+        <ListItem button>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText primary={'Browser channels'}/>
+          </ListItem>
+        <Divider />
+
+        <List component="nav" aria-labelledby="nested-list-subheader" subheader={
+          <ListSubheader component="div" id="nested-list-subheader" className={classes.subheader}>
+            MORE FROM YOUTUBE
+          </ListSubheader>
+        }>
+          
+          <ListItem button>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText primary={'YouTube Premium'}/>
+          </ListItem>
+          
+          <ListItem button>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText primary={'Live'}/>
+          </ListItem>
+
+          <Divider />
+
+          <ListItem button>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText primary={'Settings'}/>
+          </ListItem>
+          
+          <ListItem button>
+            <ListItemIcon>
+              <AddCircle />
+            </ListItemIcon>
+            <ListItemText primary={'Report history'}/>
+          </ListItem>
+
+        </List>
+
       </Drawer>
     </Box>
   </div>;
