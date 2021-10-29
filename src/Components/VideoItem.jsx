891fc68063e488a35d34359d556261888c9f1172
henriqueholtz <henrique_holtz@hotmail.com>
2021-10-28 22:17:01 -0300

    Remove unused imports (error into github actions)

--- src/Components/VideoItem.jsx
+++ src/Components/VideoItem.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, ListSubheader, Grid } from "@material-ui/core";
+import { Box, Typography } from "@material-ui/core";
 
 export default function VideoItem({video}) {
     return (
